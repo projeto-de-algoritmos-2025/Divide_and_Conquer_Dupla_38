db68cc6a8d173b523423f3aa8146329db7f5f3b9
Gabriel Flores <gabrielfcoelho19@gmail.com>
2025-11-10 08:38:51 -0300

    315: implementa merge com contagem de inversões

--- 315.count_of_smaller_numbers_after_self.cpp
+++ 315.count_of_smaller_numbers_after_self.cpp
@@ -5,6 +5,32 @@ using namespace std;
 class Solution {
     using Item = pair<int, int>;
 
+    void merge(vector<Item>& vec, int l, int mid, int r, vector<int>& counts) {
+        vector<Item> temp(r - l + 1);
+        int i = l, j = mid + 1, k = 0, right_count = 0;
+
+        while (i <= mid && j <= r) {
+            if (vec[i].first <= vec[j].first) {
+                counts[vec[i].second] += right_count;
+                temp[k++] = vec[i++];
+            } 
+            else {
+                temp[k++] = vec[j++];
+                right_count++;
+            }
+        }
+        while (i <= mid) {
+            counts[vec[i].second] += right_count;
+            temp[k++] = vec[i++];
+        }
+        while (j <= r) {
+            temp[k++] = vec[j++];
+        }
+        for (int p = 0; p < temp.size(); ++p) {
+            vec[l + p] = temp[p];
+        }
+    }
+
     void mergeSort(vector<Item>& vec, int l, int r, vector<int>& counts) {
         if (l >= r) {
             return;
