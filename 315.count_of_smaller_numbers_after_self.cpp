2578c3e564b004fe5c0cd3abcce5b528a4adb7bd
Gabriel Flores <gabrielfcoelho19@gmail.com>
2025-11-10 08:25:23 -0300

    315: adiciona a função mergesort

--- 315.count_of_smaller_numbers_after_self.cpp
+++ 315.count_of_smaller_numbers_after_self.cpp
@@ -4,6 +4,16 @@ using namespace std;
 
 class Solution {
     using Item = pair<int, int>;
+
+    void mergeSort(vector<Item>& vec, int l, int r, vector<int>& counts) {
+        if (l >= r) {
+            return;
+        }
+        int mid = l + (r - l) / 2;
+        mergeSort(vec, l, mid, counts); 
+        mergeSort(vec, mid + 1, r, counts);
+        merge(vec, l, mid, r, counts); 
+    }
 public:
     vector<int> countSmaller(vector<int>& nums) {
         int n = nums.size();
